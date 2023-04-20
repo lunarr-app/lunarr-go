e362aae97b67860f6911c947966a93189f76e1d2
Sayem Chowdhury <sayem314@gmail.com>
2023-04-20 15:48:00 +0600

    Initial commit

---
+++ server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(":3000"))
+}
