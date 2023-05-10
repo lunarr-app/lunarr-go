9f0454ae5c1c4a9026dbcbd0c5e166d9bbf0b2f2
Sayem Chowdhury <sayem314@gmail.com>
2023-05-10 14:51:03 +0600

    set defaults

--- internal/models/users.go
+++ internal/models/users.go
@@ -4,5 +4,5 @@ type UserSignup struct {
 	Displayname string `json:"displayname" validate:"required,min=1,max=48" bson:"displayname"`
 	Username    string `json:"username" validate:"required,min=2,max=16,alphanum" bson:"username"`
 	Password    string `json:"password" validate:"required,min=6,max=32" bson:"password"`
-	Sex         string `json:"sex" validate:"oneof=male female unknown" bson:"sex"`
+	Sex         string `json:"sex,omitempty" validate:"oneof=male female unknown" bson:"sex,omitempty,default=unknown"`
 }
