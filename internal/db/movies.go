6cad40e633917f6dab3f2c2872493110cbc43ef6
Sayem Chowdhury <sayem314@gmail.com>
2023-05-17 20:57:07 +0600

    db insert movie

--- internal/db/movies.go
+++ internal/db/movies.go
@@ -2,9 +2,17 @@ package db
 
 import (
 	"context"
+	"time"
 
+	TMDb "github.com/lunarr-app/golang-tmdb"
+	"github.com/lunarr-app/lunarr-go/internal/util"
 	"go.mongodb.org/mongo-driver/bson"
 )
+
+type MovieWithFiles struct {
+	Movie *TMDb.MovieDetails `bson:"movie"`
+	Files []string           `bson:"files"`
+}
 
 func CheckMovieExists(filePath string) bool {
 	filter := bson.M{
@@ -18,4 +26,23 @@ func CheckMovieExists(filePath string) bool {
 	}
 
 	return result != nil
+}
+
+func InsertMovie(movie *TMDb.MovieDetails, file string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	movieWithFiles := MovieWithFiles{
+		Movie: movie,
+		Files: []string{file},
+	}
+
+	_, err := MoviesLists.InsertOne(ctx, movieWithFiles)
+	if err != nil {
+		util.Logger.Error().Err(err).Msg("Failed to insert movie into MongoDB")
+		return err
+	}
+
+	util.Logger.Info().Msgf("Movie inserted successfully: %s", movie.Title)
+	return nil
 }
