e984b1abb1add48a5ada4b4afd2c0013134911c0
Sayem Chowdhury <sayem314@gmail.com>
2023-05-19 13:33:56 +0600

    user settings

--- internal/models/users.go
+++ internal/models/users.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type UserRole string
 
@@ -23,14 +27,33 @@ type UserSignup struct {
 }
 
 type UserMongo struct {
-	Displayname   string    `bson:"displayname" validate:"required,min=1,max=48"`
-	Username      string    `bson:"username" validate:"required,min=2,max=16,alphanum,usernamepattern"`
-	Password      string    `bson:"password" validate:"required,min=6,max=32"`
-	Sex           string    `bson:"sex" validate:"required,oneof=male female unknown"`
-	Role          UserRole  `bson:"role" validate:"required,oneof=admin superuser subscriber"`
-	APIKey        string    `bson:"api_key" validate:"required,min=32,max=32"`
-	CreatedAt     time.Time `bson:"created_at"`
-	UpdatedAt     time.Time `bson:"updated_at"`
-	LastSeenAt    time.Time `bson:"last_seen_at"`
-	CurrentStatus string    `bson:"current_status" validate:"omitempty,oneof=active restricted disabled banned"`
+	ID            primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Displayname   string             `bson:"displayname" validate:"required,min=1,max=48"`
+	Username      string             `bson:"username" validate:"required,min=2,max=16,alphanum,usernamepattern"`
+	Password      string             `bson:"password" validate:"required,min=6,max=32"`
+	Sex           string             `bson:"sex" validate:"required,oneof=male female unknown"`
+	Role          UserRole           `bson:"role" validate:"required,oneof=admin superuser subscriber"`
+	APIKey        string             `bson:"api_key" validate:"required,min=32,max=32"`
+	CurrentStatus string             `bson:"current_status" validate:"omitempty,oneof=active restricted disabled banned"`
+	Settings      UserSettings       `bson:"settings" validate:"dive"`
+	LastSeenAt    time.Time          `bson:"last_seen_at"`
+	CreatedAt     time.Time          `bson:"created_at"`
+	UpdatedAt     time.Time          `bson:"updated_at"`
+}
+
+type UserSettings struct {
+	Theme       string              `bson:"theme" validate:"required,oneof=system dark light" default:"system"`
+	Subtitle    SubtitleSettings    `bson:"subtitle" validate:"dive"`
+	Transcoding TranscodingSettings `bson:"transcoding" validate:"dive"`
+}
+
+type SubtitleSettings struct {
+	Enabled  bool   `bson:"enabled" default:"true"`
+	Language string `bson:"language" validate:"required,bcp47_language_tag" default:"en-US"`
+}
+
+type TranscodingSettings struct {
+	Resolution string `bson:"resolution" validate:"required,oneof=direct 480p 720p 1080p 4K" default:"direct"`
+	Bitrate    int    `bson:"bitrate" validate:"required" default:"2000"`
+	Codec      string `bson:"codec" validate:"required,oneof=h264 h265" default:"h264"`
 }
