886730fca8b855d9b8b7030deb9916a100b6954b
Sayem Chowdhury <sayem314@gmail.com>
2023-05-30 15:29:28 +0600

    enhance scanner

--- internal/scanner/scan.go
+++ internal/scanner/scan.go
@@ -3,8 +3,10 @@ package scanner
 import (
 	"io/fs"
 	"path/filepath"
+	"strconv"
 
 	"github.com/lunarr-app/lunarr-go/internal/db"
+	"github.com/lunarr-app/lunarr-go/internal/tmdb"
 	"github.com/lunarr-app/lunarr-go/internal/util"
 	PTN "github.com/middelink/go-parse-torrent-name"
 )
@@ -21,34 +23,65 @@ func ScanMediaDirectory(directory string) {
 			return nil
 		}
 
-		// Check if the file has a valid video extension
-		if IsValidVideoFile(path) {
-			if db.CheckMovieExists(path) {
-				util.Logger.Info().Str("path", path).Msg("Movie already exists in the database")
-				return nil
-			}
-
-			// Parse filename
-			tor, err := PTN.Parse(path)
-			if err != nil {
-				util.Logger.Err(err).Msg("Filename parse error")
-				return nil
-			}
-
-			// Check if the file is a movie by comparing the year, season, and episode
-			// First-ever movie was created in 1888, so we consider it a movie if the year is greater than or equal to 1888
-			isMovie := tor.Year >= 1888 && tor.Season == 0 && tor.Episode == 0
-			if isMovie {
-				// TODO: Find movies on TMDb
-				// TODO: Add movie to the database
-				util.Logger.Info().Str("path", path).Msg("Movie detected")
-			}
+		if !IsValidVideoFile(path) {
+			util.Logger.Warn().Str("path", path).Msg("Invalid video file, skipping")
+			return nil
 		}
 
+		if db.CheckMovieExists(path) {
+			util.Logger.Info().Str("path", path).Msg("Movie already exists in the database")
+			return nil
+		}
+
+		// Parse filename
+		tor, err := PTN.Parse(path)
+		if err != nil {
+			util.Logger.Err(err).Str("path", path).Msg("Filename parse error")
+			return nil
+		}
+
+		// Check if the file is a movie by comparing the year, season, and episode
+		// First-ever movie was created in 1888, so we consider it a movie if the year is greater than or equal to 1888
+		isMovie := tor.Year >= 1888 && tor.Season == 0 && tor.Episode == 0
+		if !isMovie {
+			util.Logger.Info().Str("path", path).Msg("Not a movie, skipping")
+			return nil
+		}
+
+		util.Logger.Info().Str("path", path).Msg("Movie detected")
+
+		// Search movies on TMDb
+		movies, err := tmdb.TmdbClient.GetSearchMovies(tor.Title, map[string]string{
+			"year":          strconv.Itoa(tor.Year),
+			"include_adult": "true",
+		})
+		if err != nil {
+			util.Logger.Err(err).Str("path", path).Msg("Failed to search movies on TMDb")
+			return nil
+		}
+
+		if movies.TotalResults == 0 {
+			util.Logger.Info().Str("path", path).Msg("No matching movies found on TMDb")
+			return nil
+		}
+
+		util.Logger.Info().Int("count", int(movies.TotalResults)).Str("path", path).Msg("Found matching movies on TMDb")
+
+		//  Get details for result on TMDb
+		movie, err := tmdb.TmdbClient.GetMovieDetails(int(movies.Results[0].ID), map[string]string{
+			"append_to_response": "keywords,alternative_titles,changes,credits,images,keywords,lists,releases,reviews,similar,translations,videos",
+		})
+		if err != nil {
+			util.Logger.Err(err).Str("path", path).Msg("Failed to search movies on TMDb")
+			return nil
+		}
+
+		//  Add movie to the database
+		db.InsertMovie(movie, path)
 		return nil
 	})
 
 	if err != nil {
-		util.Logger.Fatal().Err(err).Msg("Failed to scan media directory")
+		util.Logger.Err(err).Msg("Failed to scan media directory")
 	}
 }
