5e8b1657d3345a2a7f2335d611c22e610e6bed4b
Sayem Chowdhury <sayem314@gmail.com>
2023-06-04 00:47:59 +0600

    update server app

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,84 +1,73 @@
 package server
 
 import (
-	"io/fs"
-
-	"github.com/kataras/iris/v12"
-	"github.com/kataras/iris/v12/middleware/logger"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/template/handlebars/v2"
 
 	"github.com/lunarr-app/lunarr-go/internal/handlers"
 	"github.com/lunarr-app/lunarr-go/internal/handlers/auth"
 	"github.com/lunarr-app/lunarr-go/internal/handlers/movies"
 	"github.com/lunarr-app/lunarr-go/internal/server/middleware"
 	"github.com/lunarr-app/lunarr-go/internal/tmdb"
-	"github.com/lunarr-app/lunarr-go/internal/util"
 	"github.com/lunarr-app/lunarr-go/web"
 	"github.com/lunarr-app/lunarr-go/web/router"
 )
 
-func New() *iris.Application {
-	// Create a new Iris application
-	app := iris.New()
+func New() *fiber.App {
+	// Create a new Fiber application
+	engine := handlebars.New("./web/views", ".hbs")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 
 	// Set up a custom logger
 	customLogger := logger.New(logger.Config{
-		Status:           true,
-		IP:               true,
-		Method:           true,
-		Path:             true,
-		PathAfterHandler: true,
-		Query:            true,
+		Format:     "${status} ${ip} ${method} ${path} - ${latency}\n",
+		TimeFormat: "2006-01-02 15:04:05",
 	})
 
 	// Register the custom logger as middleware
 	app.Use(customLogger)
 
-	// Register view engine
-	views, err := fs.Sub(web.ViewsFS, "views")
-	if err != nil {
-		util.Logger.Fatal().Err(err).Msg("Failed to load web/views")
-	}
-	tmpl := iris.Handlebars(views, ".hbs")
-	app.RegisterView(tmpl)
-
 	// Serve static files
-	app.HandleDir("/assets", web.AssetsFS)
+	app.Static("/assets", "./assets")
 
 	// Register web routes
 	app.Get("/login", router.LoginPage)
 	app.Get("/signup", router.SignupPage)
 
 	// Create a sub-router for authenticated web routes
-	fe := app.Party("/")
+	fe := app.Group("/")
 	fe.Use(middleware.AuthenticateWeb)
 
 	// Register authenticated web routes
 	fe.Get("/", router.RootRedirect)
 	fe.Get("/movies", router.MoviePage)
-	fe.Get("/movies/{tmdb_id}", router.MovieDetailsPage)
+	fe.Get("/movies/:tmdb_id", router.MovieDetailsPage)
 
 	// Create a sub-router for auth
-	ha := app.Party("/auth")
+	ha := app.Group("/auth")
 	ha.Post("/signup", auth.SignupHandler)
 	ha.Post("/login", auth.LoginHandler)
 
 	// Create a sub-router for authenticated API routes
-	api := app.Party("/api")
+	api := app.Group("/api")
 	api.Use(middleware.AuthenticateAPI)
 
 	// Register authenticated API routes
 	api.Get("/", handlers.RootHandler)
 	api.Get("/movies", movies.ListsHandler)
-	api.Get("/movies/{tmdb_id}/stream", movies.MovieStreamHandler)
+	api.Get("/movies/:tmdb_id/stream", movies.MovieStreamHandler)
 
-	// // Route to render error pages
-	app.OnErrorCode(iris.StatusNotFound, router.NotFoundPage)
-	app.OnErrorCode(iris.StatusInternalServerError, router.InternalServerErrorPage)
+	// Route to render error pages
+	app.Use(router.NotFoundPage)
+	app.Use(router.InternalServerErrorPage)
 
-	// Define handlebars helper functions
-	tmpl.AddFunc("TMDbGetImageURL", tmdb.GetImageURL)
-	tmpl.AddFunc("TMDbFormatReleaseDate", tmdb.FormatReleaseDate)
-	tmpl.AddFunc("IncludeFile", web.IncludeFile)
+	// Define custom template functions
+	engine.AddFunc("TMDbGetImageURL", tmdb.GetImageURL)
+	engine.AddFunc("TMDbFormatReleaseDate", tmdb.FormatReleaseDate)
+	engine.AddFunc("IncludeFile", web.IncludeFile)
 
 	// Return the application instance
 	return app
