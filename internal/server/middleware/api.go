a302f160cf671a80ac8381dd7331371dd29df63c
Sayem Chowdhury <sayem314@gmail.com>
2023-06-03 23:37:25 +0600

    update server middleware

--- internal/server/middleware/api.go
+++ internal/server/middleware/api.go
@@ -3,46 +3,43 @@ package middleware
 import (
 	"net/http"
 
-	"github.com/kataras/iris/v12"
+	"github.com/gofiber/fiber/v2"
 	"github.com/lunarr-app/lunarr-go/internal/db"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // Authenticate middleware checks if a valid API key is provided in the header
 // and if it belongs to a valid user in the database
-func AuthenticateAPI(ctx iris.Context) {
+func AuthenticateAPI(ctx *fiber.Ctx) error {
 	// Get the API key from the header
-	apiKey := ctx.GetHeader("x-api-key")
+	apiKey := ctx.Get("x-api-key")
 
 	// Check if the API key is empty
 	if apiKey == "" {
-		ctx.StopWithJSON(http.StatusUnauthorized, iris.Map{
+		return ctx.Status(http.StatusUnauthorized).JSON(fiber.Map{
 			"status":  http.StatusText(http.StatusUnauthorized),
 			"message": "API key not provided",
 		})
-		return
 	}
 
 	// Get the user associated with the API key
 	user, err := db.GetUserByAPIKey(apiKey)
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
-			ctx.StopWithJSON(http.StatusUnauthorized, iris.Map{
+			return ctx.Status(http.StatusUnauthorized).JSON(fiber.Map{
 				"status":  http.StatusText(http.StatusUnauthorized),
 				"message": "Invalid API key",
 			})
-			return
 		}
 
-		ctx.StopWithJSON(http.StatusInternalServerError, iris.Map{
+		return ctx.Status(http.StatusInternalServerError).JSON(fiber.Map{
 			"status":  http.StatusText(http.StatusInternalServerError),
 			"message": "Failed to authenticate",
 		})
-		return
 	}
 
 	// Set the authenticated user in the context
-	ctx.Values().Set("user", user)
+	ctx.Locals("user", user)
 
-	ctx.Next()
+	return ctx.Next()
 }
