eb921a9b7868cc5b1428992cf671a9775c798476
Sayem Chowdhury <sayem314@gmail.com>
2023-06-03 22:44:22 +0600

    update handler login

--- internal/handlers/auth/login.go
+++ internal/handlers/auth/login.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
-	"github.com/kataras/iris/v12"
+	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
 
@@ -12,63 +12,58 @@ import (
 	"github.com/lunarr-app/lunarr-go/internal/models"
 )
 
-func LoginHandler(ctx iris.Context) {
+func LoginHandler(c *fiber.Ctx) error {
 	var loginReq models.UserLogin
-	if err := ctx.ReadJSON(&loginReq); err != nil {
-		ctx.StopWithJSON(http.StatusBadRequest, iris.Map{
+	if err := c.BodyParser(&loginReq); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
 			"status":  http.StatusText(http.StatusBadRequest),
 			"message": err.Error(),
 		})
-		return
 	}
 
 	// Validate user input
 	validate := validator.New()
 	if err := validate.Struct(loginReq); err != nil {
-		ctx.StopWithJSON(http.StatusBadRequest, iris.Map{
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
 			"status":  http.StatusText(http.StatusBadRequest),
 			"message": err.Error(),
 		})
-		return
 	}
 
 	// Find user in database
 	user, err := db.FindUserByUsername(loginReq.Username)
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
-			ctx.StopWithJSON(http.StatusBadRequest, iris.Map{
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{
 				"status":  http.StatusText(http.StatusBadRequest),
 				"message": "Invalid username or password",
 			})
-			return
 		} else {
-			ctx.StopWithJSON(http.StatusInternalServerError, iris.Map{
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
 				"status":  http.StatusText(http.StatusInternalServerError),
 				"message": "Failed to find user",
 			})
-			return
 		}
 	}
 
 	// Compare password hash
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginReq.Password)); err != nil {
-		ctx.StopWithJSON(http.StatusBadRequest, iris.Map{
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
 			"status":  http.StatusText(http.StatusBadRequest),
 			"message": "Invalid username or password",
 		})
-		return
 	}
 
 	// Set API key on a cookie
-	ctx.SetCookie(&http.Cookie{
+	cookie := fiber.Cookie{
 		Name:     "api_key",
 		Value:    user.APIKey,
 		Path:     "/",
-		HttpOnly: true,
+		HTTPOnly: true,
 		MaxAge:   86400 * 30, // 30 days in seconds
-		SameSite: http.SameSiteStrictMode,
-	})
+		SameSite: "Strict",
+	}
+	c.Cookie(&cookie)
 
-	ctx.StatusCode(http.StatusOK)
-	ctx.JSON(map[string]string{"status": http.StatusText(http.StatusOK), "api_key": user.APIKey})
+	return c.Status(http.StatusOK).JSON(fiber.Map{"status": http.StatusText(http.StatusOK), "api_key": user.APIKey})
 }
