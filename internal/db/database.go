63cd1b92d3881020ddf916824a475ec2132bf8d6
Sayem Chowdhury <sayem314@gmail.com>
2023-06-05 17:23:26 +0600

    update database

--- internal/db/database.go
+++ internal/db/database.go
@@ -1,42 +1,40 @@
 package db
 
 import (
-	"context"
-	"time"
-
 	"github.com/lunarr-app/lunarr-go/internal/config"
+	"github.com/lunarr-app/lunarr-go/internal/models"
 	"github.com/lunarr-app/lunarr-go/internal/util"
 
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
 )
 
-var UsersAccounts *mongo.Collection
-var MoviesLists *mongo.Collection
-var TvShowsLists *mongo.Collection
-var WatchHistory *mongo.Collection
+var DB *gorm.DB
 
 func InitDatabase() {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+	util.Logger.Info().Msg("Connecting to the SQLite database")
 
-	util.Logger.Info().Msg("Creating new MongoClient instance")
-	clientOptions := options.Client().ApplyURI(config.Get().Database.URI)
-	mongoClient, err := mongo.Connect(ctx, clientOptions)
+	// Connect to the SQLite database
+	db, err := gorm.Open(sqlite.Open(config.Get().Database.URI), &gorm.Config{})
 	if err != nil {
-		util.Logger.Fatal().Err(err).Msg("Failed to connect to MongoDB")
-	}
-	err = mongoClient.Ping(ctx, nil)
-	if err != nil {
-		util.Logger.Fatal().Err(err).Msg("Failed to ping MongoDB")
+		util.Logger.Fatal().Err(err).Msg("Failed to connect to the SQLite database")
 	}
 
-	util.Logger.Info().Msg("Exporting MongoDB collections as typed objects")
-	UsersAccounts = mongoClient.Database("lunarr").Collection("users.accounts")
-	MoviesLists = mongoClient.Database("lunarr").Collection("movies.lists")
-	TvShowsLists = mongoClient.Database("lunarr").Collection("tv_shows.lists")
-	WatchHistory = mongoClient.Database("lunarr").Collection("watch.history")
+	// Set the database connection in the DB variable
+	DB = db
 
-	// Generate index
-	CreateIndexes()
+	// AutoMigrate the tables
+	MigrateTables()
+
+	util.Logger.Info().Msg("Database initialization complete")
+}
+
+func MigrateTables() {
+	err := DB.AutoMigrate(
+		&models.UserAccount{},
+		&models.MovieWithFiles{},
+	)
+	if err != nil {
+		util.Logger.Fatal().Err(err).Msg("Failed to perform auto migration")
+	}
 }
