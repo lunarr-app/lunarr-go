1e8d72771515af682c339ad745a32a9cc44b9a7f
Sayem Chowdhury <sayem314@gmail.com>
2023-06-05 17:15:39 +0600

    update models

--- internal/models/movies.go
+++ internal/models/movies.go
@@ -1,14 +1,12 @@
 package models
 
 import (
-	"time"
-
 	TMDb "github.com/lunarr-app/golang-tmdb"
+	"gorm.io/gorm"
 )
 
 type MovieWithFiles struct {
-	Movie     *TMDb.MovieDetails `json:"movie" bson:"movie"`
-	Files     []string           `json:"files" bson:"files"`
-	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
-	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
+	gorm.Model
+	Movie TMDb.MovieDetails `gorm:"embedded" json:"movie"`
+	Files []string          `gorm:"type:text[]" json:"files"`
 }
--- internal/models/settings.go
+++ internal/models/settings.go
@@ -1,23 +1,19 @@
 package models
 
 import (
-	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"gorm.io/gorm"
 )
 
 type AppSettings struct {
-	ID              primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
-	MovieLocations  []string           `json:"movie_locations" bson:"movie_locations"`
-	TVShowLocations []string           `json:"tv_show_locations" bson:"tv_show_locations"`
-	EmailSMTP       *EmailSMTPSettings `json:"email_smtp" bson:"email_smtp,omitempty"`
-	CreatedAt       time.Time          `json:"created_at" bson:"created_at"`
-	UpdatedAt       time.Time          `json:"updated_at" bson:"updated_at"`
+	gorm.Model
+	MovieLocations  []string           `json:"movie_locations" gorm:"column:movie_locations"`
+	TVShowLocations []string           `json:"tv_show_locations" gorm:"column:tv_show_locations"`
+	EmailSMTP       *EmailSMTPSettings `json:"email_smtp" gorm:"embedded"`
 }
 
 type EmailSMTPSettings struct {
-	SMTPServer string `json:"smtp_server" bson:"smtp_server"`
-	Port       int    `json:"port" bson:"port"`
-	Username   string `json:"username" bson:"username"`
-	Password   string `json:"password" bson:"password"`
+	SMTPServer string `json:"smtp_server" gorm:"column:smtp_server"`
+	Port       int    `json:"port" gorm:"column:port"`
+	Username   string `json:"username" gorm:"column:username"`
+	Password   string `json:"password" gorm:"column:password"`
 }
