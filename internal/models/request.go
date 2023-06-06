ccec604b730c390de746f6da05c626a1e35731b7
Sayem Chowdhury <sayem314@gmail.com>
2023-06-06 23:40:51 +0600

    update name

--- internal/models/request.go
+++ internal/models/request.go
@@ -1,13 +1,13 @@
 package models
 
-type Filter struct {
+type SearchQueryParamsFilter struct {
 	Field string `json:"field,omitempty" validate:"oneof=title genres year"`
 	Value string `json:"value,omitempty" validate:"required" default:""`
 }
 
 type SearchQueryParams struct {
-	Limit   int      `json:"limit,omitempty" validate:"gte=1" default:"1"`
-	Page    int      `json:"page,omitempty" validate:"gte=1" default:"1"`
-	Search  string   `json:"search,omitempty" default:""`
-	Filters []Filter `json:"filters,omitempty" validate:"dive"`
+	Limit   int                       `json:"limit,omitempty" validate:"gte=1" default:"1"`
+	Page    int                       `json:"page,omitempty" validate:"gte=1" default:"1"`
+	Search  string                    `json:"search,omitempty" default:""`
+	Filters []SearchQueryParamsFilter `json:"filters,omitempty" validate:"dive"`
 }
