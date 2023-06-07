b86c683eeca8cc797d765663b10bda042380389a
Sayem Chowdhury <sayem314@gmail.com>
2023-06-07 18:43:27 +0600

    refactor scanner

---
+++ internal/scanner/movie.go
@@ -0,0 +1,48 @@
+package scanner
+
+import (
+	"strconv"
+
+	PTN "github.com/middelink/go-parse-torrent-name"
+
+	"github.com/lunarr-app/lunarr-go/internal/db"
+	"github.com/lunarr-app/lunarr-go/internal/tmdb"
+	"github.com/lunarr-app/lunarr-go/internal/util"
+)
+
+func processMovie(filename string, tor *PTN.TorrentInfo, path string) {
+	util.Logger.Info().Str("filename", filename).Msg("Movie detected")
+
+	// Search movies on TMDb
+	movies, err := tmdb.TmdbClient.GetSearchMovies(tor.Title, map[string]string{
+		"year":          strconv.Itoa(tor.Year),
+		"include_adult": "true",
+	})
+	if err != nil {
+		util.Logger.Err(err).Str("filename", filename).Msg("Failed to search movies on TMDb")
+		return
+	}
+
+	if movies.TotalResults == 0 {
+		util.Logger.Info().Str("filename", filename).Msg("No matching movies found on TMDb")
+		return
+	}
+
+	util.Logger.Info().Int("count", int(movies.TotalResults)).Str("filename", filename).Msg("Found matching movies on TMDb")
+
+	// Get details for result on TMDb
+	movie, err := tmdb.TmdbClient.GetMovieDetails(int(movies.Results[0].ID), nil)
+	if err != nil {
+		util.Logger.Err(err).Str("filename", filename).Msg("Failed to search movies on TMDb")
+		return
+	}
+
+	// Add movie to the database
+	err = db.InsertMovie(movie, path)
+	if err != nil {
+		util.Logger.Err(err).Str("filename", filename).Msg("Failed to insert movie into MongoDB")
+		return
+	}
+
+	util.Logger.Info().Msgf("Movie inserted successfully: %s", movie.Title)
+}
--- internal/scanner/scan.go
+++ internal/scanner/scan.go
@@ -3,12 +3,11 @@ package scanner
 import (
 	"io/fs"
 	"path/filepath"
-	"strconv"
+
+	PTN "github.com/middelink/go-parse-torrent-name"
 
 	"github.com/lunarr-app/lunarr-go/internal/db"
-	"github.com/lunarr-app/lunarr-go/internal/tmdb"
 	"github.com/lunarr-app/lunarr-go/internal/util"
-	PTN "github.com/middelink/go-parse-torrent-name"
 )
 
 func ScanMediaDirectory(directory string) {
@@ -45,45 +44,12 @@ func ScanMediaDirectory(directory string) {
 		// Check if the file is a movie by comparing the year, season, and episode
 		// First-ever movie was created in 1888, so we consider it a movie if the year is greater than or equal to 1888
 		isMovie := tor.Year >= 1888 && tor.Season == 0 && tor.Episode == 0
-		if !isMovie {
+		if isMovie {
+			processMovie(filename, tor, path)
+		} else {
 			util.Logger.Info().Str("filename", filename).Msg("Not a movie, skipping")
-			return nil
 		}
 
-		util.Logger.Info().Str("filename", filename).Msg("Movie detected")
-
-		// Search movies on TMDb
-		movies, err := tmdb.TmdbClient.GetSearchMovies(tor.Title, map[string]string{
-			"year":          strconv.Itoa(tor.Year),
-			"include_adult": "true",
-		})
-		if err != nil {
-			util.Logger.Err(err).Str("filename", filename).Msg("Failed to search movies on TMDb")
-			return nil
-		}
-
-		if movies.TotalResults == 0 {
-			util.Logger.Info().Str("filename", filename).Msg("No matching movies found on TMDb")
-			return nil
-		}
-
-		util.Logger.Info().Int("count", int(movies.TotalResults)).Str("filename", filename).Msg("Found matching movies on TMDb")
-
-		// Get details for result on TMDb
-		movie, err := tmdb.TmdbClient.GetMovieDetails(int(movies.Results[0].ID), nil)
-		if err != nil {
-			util.Logger.Err(err).Str("filename", filename).Msg("Failed to search movies on TMDb")
-			return nil
-		}
-
-		// Add movie to the database
-		err = db.InsertMovie(movie, path)
-		if err != nil {
-			util.Logger.Err(err).Str("filename", filename).Msg("Failed to insert movie into MongoDB")
-			return nil
-		}
-
-		util.Logger.Info().Msgf("Movie inserted successfully: %s", movie.Title)
 		return nil
 	})
 
