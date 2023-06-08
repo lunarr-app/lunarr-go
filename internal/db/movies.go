55112f91f5267cde0e788c528d0db84d1187aab4
Sayem Chowdhury <sayem314@gmail.com>
2023-06-08 16:00:29 +0600

    add badger retrieval

--- internal/db/movies.go
+++ internal/db/movies.go
@@ -61,6 +61,28 @@ func FindMovieByTmdbID(tmdbID int) (*models.MovieWithFiles, error) {
 	return &movie, nil
 }
 
+func FindMovieMetadata(tmdbID int) (*TMDb.MovieDetails, error) {
+	// Retrieve the movie data from Badger using the key format "tmdb_movie:<tmdbID>"
+	var movieData *TMDb.MovieDetails
+	err := BadgerDB.View(func(txn *badger.Txn) error {
+		key := "tmdb_movie:" + strconv.Itoa(tmdbID)
+		item, err := txn.Get([]byte(key))
+		if err != nil {
+			return err
+		}
+
+		err = item.Value(func(val []byte) error {
+			return json.Unmarshal(val, &movieData)
+		})
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return movieData, nil
+}
+
 func DeleteMovieByTmdbID(tmdbID int) error {
 	err := DB.Delete(&models.MovieWithFiles{}, "tmdb_id = ?", tmdbID).Error
 	if err != nil {
