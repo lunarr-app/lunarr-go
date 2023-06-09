c9bdee486c523085904cb79f6026150a1f724a46
Sayem Chowdhury <sayem314@gmail.com>
2023-06-09 22:43:16 +0600

    update model

--- internal/models/settings.go
+++ internal/models/settings.go
@@ -9,11 +9,12 @@ type AppSettings struct {
 	MovieLocations  StringArray        `json:"movie_locations" gorm:"column:movie_locations;type:text[]"`
 	TVShowLocations StringArray        `json:"tv_show_locations" gorm:"column:tv_show_locations;type:text[]"`
 	EmailSMTP       *EmailSMTPSettings `json:"email_smtp" gorm:"embedded"`
+	NewUserSignup   bool               `json:"new_user_signup" gorm:"column:new_user_signup;default:true"`
 }
 
 type EmailSMTPSettings struct {
 	SMTPServer string `json:"smtp_server" gorm:"column:smtp_server"`
-	Port       int    `json:"port" gorm:"column:port"`
-	Username   string `json:"username" gorm:"column:username"`
-	Password   string `json:"password" gorm:"column:password"`
+	Port       int    `json:"smtp_port" gorm:"column:smtp_port"`
+	Username   string `json:"smtp_username" gorm:"column:smtp_username"`
+	Password   string `json:"smtp_password" gorm:"column:smtp_password"`
 }
