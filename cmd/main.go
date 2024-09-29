76eb3e6f8cadb94175f98ae27cc5ef1c0d457922
Sayem Chowdhury <sayem314@gmail.com>
2024-09-29 13:34:25 +0600

    update zerolog

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 
 	"github.com/lunarr-app/lunarr-go/internal/config"
 	"github.com/lunarr-app/lunarr-go/internal/db"
@@ -10,16 +14,17 @@ import (
 	"github.com/lunarr-app/lunarr-go/internal/scanner"
 	"github.com/lunarr-app/lunarr-go/internal/server"
 	"github.com/lunarr-app/lunarr-go/internal/tmdb"
-	"github.com/lunarr-app/lunarr-go/internal/util"
 )
 
 func main() {
-	// Initialize logger instance
-	util.InitLogger()
+	// Set up zerolog for pretty console output
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.DateTime})
 
-	// Parse command-line flags
+	// Set log level to info
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	// Initialize the configuration
 	config.InitConfig()
-	config.ParseFlags()
 
 	// Initialize the database
 	db.InitDatabase()
@@ -27,30 +32,30 @@ func main() {
 	// Initialize the TMDB client
 	tmdb.InitTMDBClient()
 
-	// Get app settings from database
+	// Get app settings from the database
 	settings, err := db.GetSettings()
 	if err != nil {
-		util.Logger.Fatal().Err(err).Msg("Failed to get settings")
+		log.Fatal().Err(err).Msg("Failed to get settings")
 	}
 
-	// If settings don't exist, create new settings and insert into the database
+	// If settings don't exist, create new settings and insert them into the database
 	if settings == nil {
-		util.Logger.Info().Msg("No settings found, creating new settings")
+		log.Info().Msg("No settings found, creating new settings")
 		settings, err = db.InsertSettings(&models.AppSettings{
 			MovieLocations:  models.StringArray{},
 			TVShowLocations: models.StringArray{},
 		})
 		if err != nil {
-			util.Logger.Fatal().Err(err).Msg("Failed to insert new settings")
+			log.Fatal().Err(err).Msg("Failed to insert new settings")
 		}
 
-		util.Logger.Info().Msg("Created new settings")
+		log.Info().Msg("Created new settings")
 	}
 
 	// Scan all movie locations if they exist
 	if len(settings.MovieLocations) > 0 {
 		for _, movieLocation := range settings.MovieLocations {
-			util.Logger.Info().Str("location", movieLocation).Msg("Scanning movie location")
+			log.Info().Str("location", movieLocation).Msg("Scanning movie location")
 
 			// Start a goroutine to run ScanMediaDirectory in the background
 			go scanner.ScanMediaDirectory(movieLocation)
@@ -63,5 +68,10 @@ func main() {
 	// Start the server on the specified port
 	cfg := config.Get()
 	addr := fmt.Sprintf("%s:%d", cfg.Server.Host, cfg.Server.Port)
-	log.Fatal(app.Listen(addr))
+	log.Info().Msgf("Server starting at %s", addr)
+
+	// Improved error logging for server start
+	if err := app.Listen(addr); err != nil {
+		log.Fatal().Err(err).Msg("Failed to start server")
+	}
 }
